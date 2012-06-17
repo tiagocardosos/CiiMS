c0d71260ede066443de0b884a8b94366a0444335
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2012-06-16 22:23:06 -0500

    Google Analytics

--- themes/reducto/views/layouts/main.php
+++ themes/reducto/views/layouts/main.php
@@ -181,5 +181,20 @@
 	} catch( err ) {}
 	</script><noscript><p><img src="http://erianna.com:2080/piwik.php?idsite=2" style="border:0" alt="" /></p></noscript>
 	<!-- End Piwik Tracking Code -->   
+	<script type="text/javascript">
+	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-21577234-3']);
+	  _gaq.push(['_setDomainName', 'none']);
+	 _gaq.push(['_setAllowLinker', true]);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
+	</script>
 	</body>
 </html>
