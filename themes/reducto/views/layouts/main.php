570ba0f27262c1f6e18f82a0703dd4885db719e1
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2012-06-16 20:20:55 -0500

    Added Piwik Tracking code to main view

--- themes/reducto/views/layouts/main.php
+++ themes/reducto/views/layouts/main.php
@@ -168,6 +168,18 @@
 	        </div>        
 	    </div>
 	
-	<script type="text/javascript"> Cufon.now(); </script>    
+	<script type="text/javascript"> Cufon.now(); </script> 
+	<!-- Piwik --> 
+	<script type="text/javascript">
+	var pkBaseURL = (("https:" == document.location.protocol) ? "https://erianna.com:2080/" : "http://erianna.com:2080/");
+	document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+	</script><script type="text/javascript">
+	try {
+	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
+	piwikTracker.trackPageView();
+	piwikTracker.enableLinkTracking();
+	} catch( err ) {}
+	</script><noscript><p><img src="http://erianna.com:2080/piwik.php?idsite=2" style="border:0" alt="" /></p></noscript>
+	<!-- End Piwik Tracking Code -->   
 	</body>
 </html>
