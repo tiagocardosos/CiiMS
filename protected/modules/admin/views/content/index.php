9cea6b9d46ea81a7f8ce4262ea04487d539b2638
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2012-06-27 22:40:34 -0500

    Readding index files

---
+++ protected/modules/admin/views/content/index.php
@@ -0,0 +1,26 @@
+<?php $this->widget('bootstrap.widgets.BootGridView',array(
+	'id'=>'content-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'id',
+		'author_id'=>array(
+			'header'=>'Author',
+			'value'=>'Users::model()->findByPk($data->author_id)->displayName'
+		),
+		'title',
+		'status',
+		'parent_id',
+		'category_id'=>array(
+			'header'=>'Category', 
+			'value'=>'Categories::model()->findByPk($data->category_id)->name'
+		),
+		'comment_count',
+		array(
+			'class'=>'bootstrap.widgets.BootButtonColumn',
+			'viewButtonUrl'=>'Yii::app()->createUrl($data->slug)',
+			'updateButtonUrl'=>'Yii::app()->createUrl("/admin/content/save/id/" . $data->id)',
+			'deleteButtonUrl'=>'Yii::app()->createUrl("/admin/content/delete/id/" . $data->id)',
+		),
+	),
+)); ?>
---
+++ protected/modules/admin/views/settings/index.php
@@ -0,0 +1,14 @@
+<?php $this->widget('bootstrap.widgets.BootGridView',array(
+	'id'=>'configuration-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'key',
+		'value',
+		array(
+			'class'=>'bootstrap.widgets.BootButtonColumn',
+			'template'=>'{update}{delete}',
+			'updateButtonUrl'=>"Yii::app()->createUrl('/admin/settings/save')"
+		),
+	),
+)); ?>
