94831e481112f4b0e8d961f23d62c978b3d2e3e9
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2012-07-18 09:51:33 -0500

    Added AddThis to post sidebar for sharing

--- themes/reducto/views/layouts/blog.php
+++ themes/reducto/views/layouts/blog.php
@@ -24,17 +24,29 @@
                 </form>
 		</div>
 		
+		<!-- Add This -->
+		<div class="one-fourth-last">
+			<br />
+			<h5>Share This Post</h5>
+			<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+				<a class="addthis_button_facebook"></a>
+				<a class="addthis_button_linkedin"></a>
+				<a class="addthis_button_twitter"></a>
+				<a class="addthis_button_google_plusone_share" g:plusone:size="medium"></a>
+				<a class="addthis_button_preferred_5"></a>
+				<a class="addthis_button_compact"></a>
+			</div>
+			<? Yii::app()->clientScript->registerScriptFile('http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-5006c955766d11a9'); ?>
+		</div>
 		<!-- Tag Cloud -->
 		
 		<!-- Ad[s] -->
 		
 		<div class="one-fourth-last">
-   			<br />
 			<h5>More in this Category</h5>
 			<div class="horizontal-rule"></div>
 			<ul class="widget-list">
 				<?
-
 					$related = Yii::app()->db->createCommand('
 						SELECT id, title, slug, comment_count FROM content  WHERE category_id = ' . $this->params['data']['category_id'] .
 						' AND id != ' . $this->params['data']['id'] . ' AND vid = (SELECT MAX(vid) FROM content AS content2 WHERE content2.id = content.id) ORDER BY updated DESC LIMIT 5')->queryAll();
